9cba28f56cfdfa4397ac1074ffb232e18f756736
Jaidha Rosenblatt <jrr59@duke.edu>
2020-02-21 20:05:18 -0500

    initialized javafx stage

--- src/view/Visualizer.java
+++ src/view/Visualizer.java
@@ -1,5 +1,22 @@
 package view;
 
+import java.util.ResourceBundle;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Visualizer {
+  private Stage stage;
+  private static final String DEFAULT_LANGUAGE = "English";
+
+  private ResourceBundle resourceBundle = ResourceBundle.getBundle("resources/ui/"+ DEFAULT_LANGUAGE);
+
+  public Visualizer(Stage stage) {
+    this.stage = stage;
+    stage.setTitle(resourceBundle.getString("Title"));
+    Scene scene = new Scene(new Group(),500,500);
+    stage.setScene(scene);
+    stage.show();
+  }
 
 }
