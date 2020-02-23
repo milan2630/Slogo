edc424238f0f719e5113d8ad895fbfff8a21cca4
Jaidha Rosenblatt <jrr59@duke.edu>
2020-02-23 13:57:23 -0500

    switched from stackpane to anchorpane in visualizer

--- src/view/Visualizer.java
+++ src/view/Visualizer.java
@@ -6,9 +6,9 @@ import java.beans.PropertyChangeListener;
 import java.util.MissingResourceException;
 import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Visualizer implements PropertyChangeListener {
@@ -26,20 +26,35 @@ public class Visualizer implements PropertyChangeListener {
     this.language = language;
     setTitle(stage);
 
-    BorderPane borderPane = new BorderPane();
+    AnchorPane root = new AnchorPane();
 
     terminal = new Terminal(language);
     terminal.addChangeListener(this);
 
     display = new Display();
 
-    borderPane.setRight(display.getPane());
-    borderPane.setBottom(terminal.getPane());
+    addPanesToRoot(root);
 
-    Scene scene = new Scene(borderPane, SCENE_WIDTH, SCENE_HEIGHT);
+    Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
     stage.setScene(scene);
     stage.show();
 
+  }
+
+  private void addPanesToRoot(AnchorPane root) {
+    Node terminalNode = terminal.getPane();
+    AnchorPane.setBottomAnchor(terminalNode, 0.0);
+    AnchorPane.setLeftAnchor(terminalNode,0.0);
+    AnchorPane.setRightAnchor(terminalNode,0.0);
+
+    Node displayNode = display.getPane();
+    AnchorPane.setTopAnchor(displayNode,0.0);
+    AnchorPane.setBottomAnchor(displayNode,terminal.getHeight());
+    AnchorPane.setRightAnchor(displayNode,0.0);
+    //TODO replace 200 with controller width
+    AnchorPane.setLeftAnchor(displayNode,200.0);
+
+    root.getChildren().addAll(terminalNode,displayNode);
   }
 
   private void setTitle(Stage stage) {
@@ -63,7 +78,8 @@ public class Visualizer implements PropertyChangeListener {
     int r = new Random().nextInt(600);
     int r2 = new Random().nextInt(400);
 
-    System.out.println(evt.getPropertyName() + " " + evt.getNewValue());
+    System.out.println(evt.getNewValue());
     display.moveTurtle(new Point(r,r2));
+
   }
 }
