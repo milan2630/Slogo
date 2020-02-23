1d9fa43f386d2c66028a5f974e1aeb06ff41c580
Milan Bhat <mb554@duke.edu>
2020-02-23 16:41:04 -0500

    Created command to move turtle forward

--- src/slogo/ForwardCommand.java
+++ src/slogo/ForwardCommand.java
@@ -1,4 +1,28 @@
 package slogo;
 
-public class ForwardCommand {
+import java.awt.*;
+
+public class ForwardCommand implements TurtleCommand{
+
+
+    private double pixelsForward;
+
+    public ForwardCommand(double pixels){
+        pixelsForward = pixels;
+    }
+
+    public double getPixelsForward(){
+        return pixelsForward;
+    }
+
+
+    @Override
+    public int getTurtleNum() {
+        return 0;
+    }
+
+    @Override
+    public int getNumArguments() {
+        return 1;
+    }
 }
