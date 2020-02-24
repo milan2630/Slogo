e5d939f37872ce65dc58c54beb2a8aa28174a9ab
Milan Bhat <mb554@duke.edu>
2020-02-23 23:36:35 -0500

    Created backward command class

--- src/slogo/BackwardCommand.java
+++ src/slogo/BackwardCommand.java
@@ -1,4 +1,42 @@
 package slogo;
 
-public class BackwardCommand {
+import java.util.List;
+
+public class BackwardCommand implements TurtleCommand{
+
+
+    private int pixelsBackward;
+
+    public BackwardCommand(){
+        //do nothing
+    }
+
+    public BackwardCommand(int pixels){
+        pixelsBackward = pixels;
+    }
+
+    public int getPixelsBackward(){
+        return pixelsBackward;
+    }
+
+
+    @Override
+    public int getTurtleNum() {
+        return 0;
+    }
+
+    @Override
+    public int getNumArguments() {
+        return 1;
+    }
+
+    @Override
+    public void setArguments(List<Integer> arguments) {
+        pixelsBackward = arguments.get(0);
+    }
+
+    @Override
+    public int getReturn() {
+        return pixelsBackward;
+    }
 }
