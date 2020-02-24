0caa1a4b53e5c93246c96d0ae9317b781fb2e8b6
Milan Bhat <mb554@duke.edu>
2020-02-23 20:09:33 -0500

    Added set argument and get return to Interface

--- src/slogo/Command.java
+++ src/slogo/Command.java
@@ -1,10 +1,14 @@
 package slogo;
 
+import java.util.List;
+
 public interface Command {
 
     public int getNumArguments();
 
+    public void setArguments(List<String> arguments);
 
+    public int getReturn();
 
 
 
