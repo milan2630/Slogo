0cd6973830b1d7a6c551ee05c4c58630d695a4ae
Milan Bhat <mb554@duke.edu>
2020-02-23 23:12:32 -0500

    Basic parsing implementation working

--- src/slogo/Parser.java
+++ src/slogo/Parser.java
@@ -1,8 +1,7 @@
 package slogo;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class Parser {
 
@@ -32,10 +31,71 @@ public class Parser {
     private List<Command> parseLine(String line) {
         List<Command> commands = new ArrayList<>();
 
+        Stack<Integer> argumentStack = new Stack<>();
+        Stack<Command> commandStack = new Stack<>();
 
+        String[] entityList = line.split(" ");
+        for(String item: entityList){
+            try{
+                int arg = Integer.parseInt(item);
+                argumentStack.push(arg);
+            }
+            catch (NumberFormatException e){
+                try{
+                    Command com = factory.getCommand(item);
+                    commandStack.push(com);
 
+                } catch (InstantiationException ex) {
+                    ex.printStackTrace();
+                } catch (InvocationTargetException ex) {
+                    ex.printStackTrace();
+                } catch (NoSuchMethodException ex) {
+                    ex.printStackTrace();
+                } catch (IllegalAccessException ex) {
+                    ex.printStackTrace();
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            int numArguments = argumentStack.size();
+            Command topCom = commandStack.peek();
+            while(numArguments >= topCom.getNumArguments()){
+                topCom = commandStack.pop();
+                List<Integer> params = new ArrayList<>();
+                for(int i = 0; i < numArguments; i++){
+                    params.add(argumentStack.pop());
+                }
+                Collections.reverse(params);
+                topCom.setArguments(params);
+                commands.add(topCom);
+                argumentStack.add(topCom.getReturn());
+                numArguments = argumentStack.size();
+                if(commandStack.size() > 0) {
+                    topCom = commandStack.peek();
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        return commands;
 
     }
 
 
+    public static void main(String[] args) {
+        Parser t = new Parser("English");
+        String s = "fd fd 50";
+        try {
+            List<Command> x = t.parseStringToCommands(s);
+            for(Command c: x){
+                System.out.println(c.getClass() + ": " + c.getReturn());
+            }
+
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        }
+    }
 }
