a4ee91b179c2f48147668a9eaa82417e51c7dc2e
Milan Bhat <mb554@duke.edu>
2020-02-25 01:11:07 -0500

    Contains a list of methods now and can be accessed as appropriate

--- src/slogo/MethodExplorer.java
+++ src/slogo/MethodExplorer.java
@@ -1,4 +1,25 @@
 package slogo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MethodExplorer {
+
+    private Map<String, SlogoMethod> methodMap;
+
+    public MethodExplorer(){
+        methodMap = new HashMap<>();
+    }
+
+    public void addMethod(SlogoMethod method){
+        methodMap.put(method.getName(), method);
+    }
+
+    public SlogoMethod getMethod(String name){
+        if(methodMap.keySet().contains(name)){
+            return methodMap.get(name);
+        }
+        return null;
+    }
+
 }
