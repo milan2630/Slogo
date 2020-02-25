4badc1c30ca8f482c27317171e9f4c95bf407adc
Milan Bhat <mb554@duke.edu>
2020-02-25 03:44:19 -0500

    Parser now supports creation of user-defined methods

--- src/slogo/Parser.java
+++ src/slogo/Parser.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Parser {
 
 
-
+    private String language;
     private CommandFactory factory;
     private Stack<IntegerVariable> executionTimesStack;
     private IntegerVariable executionTimes;
@@ -14,8 +14,11 @@ public class Parser {
     private boolean creatingMethod;
     private MethodExplorer methodExplorer;
     private int lastReturnFromMethod;
+    private SlogoMethod currentMethod;
+    private int bracketsSeen;
 
     public Parser(String lang, MethodExplorer me){
+        language = lang;
         methodExplorer = me;
         factory = new CommandFactory(lang, methodExplorer);
         executionTimesStack = new Stack<>();
@@ -23,6 +26,7 @@ public class Parser {
         executionLimit = 1;
         creatingMethod = false;
         lastReturnFromMethod = 0;
+        bracketsSeen = 0;
     }
 
     /**
@@ -35,11 +39,50 @@ public class Parser {
         List<ImmutableTurtle> stateList = new ArrayList<>();
         String[] lineList = input.split("\n");
         for(int i = 0; i < lineList.length; i++){
-            stateList.addAll(parseLine(lineList[i], turtle));
+            if(lineList[i].indexOf("to") == 0){
+                currentMethod = createMethod(lineList, i);
+                i++;
+                bracketsSeen++;
+            }
+            else if(creatingMethod){
+                i = addLinesToMethod(lineList, i);
+            }
+            else{
+                stateList.addAll(parseLine(lineList[i], turtle));
+            }
         }
 
         //Pop next executiontime
         return stateList;
+    }
+
+    private int addLinesToMethod(String[] lineList, int i) {
+        while(bracketsSeen > 0){
+            if(lineList[i].contains("]")){
+                bracketsSeen--;
+            }
+            else{
+                if(lineList[i].contains("[")){
+                    bracketsSeen++;
+                }
+                currentMethod.addCommand(lineList[i]);
+            }
+            i++;
+        }
+        creatingMethod = false;
+        methodExplorer.addMethod(currentMethod);
+        currentMethod = null;
+        return i - 1;
+    }
+
+    private SlogoMethod createMethod(String[] lines, int curIndex) {
+        String declarationLine = lines[curIndex];
+        String[] decParts = declarationLine.split(" ");
+        String name = decParts[1];
+        String[] varNames = Arrays.copyOfRange(decParts, 3, decParts.length-1);
+        List<String> vars = Arrays.asList(varNames);
+        creatingMethod = true;
+        return new SlogoMethod(name, vars);
     }
 
     private List<ImmutableTurtle> parseLine(String line, Turtle turtle) {
@@ -92,16 +135,17 @@ public class Parser {
                 topCom.setArguments(params);
                 int result = 0;
                 if(topCom instanceof SlogoMethod){
-                    states.addAll(parseStringToCommands(((SlogoMethod) topCom).getCommandsAsString(), turtle));
+                    Parser internalParser = new Parser(language, methodExplorer);
+                    states.addAll(internalParser.parseStringToCommands(((SlogoMethod) topCom).getCommandsAsString(), turtle));
                     result = lastReturnFromMethod;
                 }
                 else {
                     result = turtle.actOnCommand(topCom);
                     states.add(turtle.getImmutableTurtle());
                 }
-                argumentStack.add(result);
-                numArguments = argumentStack.size();
                 if(commandStack.size() > 0) {
+                    argumentStack.add(result);
+                    numArguments = argumentStack.size();
                     topCom = commandStack.peek();
                 }
                 else{
@@ -110,7 +154,7 @@ public class Parser {
             }
         }
         catch(EmptyStackException | ParsingException e){
-            System.out.println("Incompatible number of commands and arguments");
+            e.printStackTrace();
         }
     }
 
@@ -120,13 +164,14 @@ public class Parser {
         Parser t = new Parser("English", me);
         Turtle turt = new Turtle();
 
-        SlogoMethod m = new SlogoMethod("NewMeth", new ArrayList<>(), new ArrayList<>());
+        /*SlogoMethod m = new SlogoMethod("NewMeth", new ArrayList<>(), new ArrayList<>());
 
         m.addCommand("fd 5");
         m.addCommand("fd fd 10");
 
-        me.addMethod(m);
-        String s = "NewMeth";
+        me.addMethod(m);*/
+        String s = "to NewMeth [ ]\n[\nfd 5 fd 5\nfd fd 10\n]\nNewMeth";
+        //String s = "fd fd 5";
         try {
             List<ImmutableTurtle> x = t.parseStringToCommands(s, turt);
             for(ImmutableTurtle c: x){
