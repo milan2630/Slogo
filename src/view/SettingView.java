00107bde2345ded299c55bf6519d3fa8ba437229
Jaidha Rosenblatt <jrr59@duke.edu>
2020-02-27 15:14:21 -0500

    made settings dark theme

--- src/view/SettingView.java
+++ src/view/SettingView.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
@@ -26,156 +27,167 @@ import java.net.URL;
 import java.util.*;
 
 public class SettingView {
-    private static ResourceBundle resourceBundle;
-    private static ResourceBundle imageBundle;
-    private static ResourceBundle languageBundle;
-    private String language;
-    private Tab myTab;
-    private List<PropertyChangeListener> listener;
-    private String penColorData;
-    private String backgroundColorData;
-    private String turtleImage;
-    private static final String PEN_COLOR = "Pen Color";
-    private static final String BACKGROUND_COLOR = "Background Color";
-    private static final String TURTLE_IMAGE = "TurtleImage";
-    private static final String PREFIX = "resources/ui/";
-    private static final String IMAGE_PATH = "TurtleImages";
-    private static final String HELP_IMAGES_PATH = "resources";
-    private static final String LANGUAGE_PATH = "src/resources/languages/";
-    private final int IMAGE_HEIGHT = 283;
-    private final int IMAGE_WIDTH = 500;
+
+  private static ResourceBundle resourceBundle;
+  private static ResourceBundle imageBundle;
+  private static ResourceBundle languageBundle;
+  private String language;
+  private Tab myTab;
+  private List<PropertyChangeListener> listener;
+  private String penColorData;
+  private String backgroundColorData;
+  private String turtleImage;
+  private static final String PEN_COLOR = "Pen Color";
+  private static final String BACKGROUND_COLOR = "Background Color";
+  private static final String TURTLE_IMAGE = "TurtleImage";
+  private static final String PREFIX = "resources/ui/";
+  private static final String IMAGE_PATH = "TurtleImages";
+  private static final String HELP_IMAGES_PATH = "resources";
+  private static final String LANGUAGE_PATH = "src/resources/languages/";
+  private final int IMAGE_HEIGHT = 283;
+  private final int IMAGE_WIDTH = 500;
   private static final double PADDING = 5;
 
-    public SettingView(String language) {
-        this.language = language;
-        resourceBundle = ResourceBundle
-                .getBundle(PREFIX + language);
-        imageBundle = ResourceBundle.getBundle(PREFIX+IMAGE_PATH);
-        myTab = new Tab(resourceBundle.getString("SettingTab"));
-        listener = new ArrayList<>();
-        this.language = language;
-        setupTab();
-    }
+  public SettingView(String language) {
+    this.language = language;
+    resourceBundle = ResourceBundle
+        .getBundle(PREFIX + language);
+    imageBundle = ResourceBundle.getBundle(PREFIX + IMAGE_PATH);
+    myTab = new Tab(resourceBundle.getString("SettingTab"));
+    listener = new ArrayList<>();
+    this.language = language;
+    setupTab();
+  }
 
-    public Tab getTab() { return myTab;}
+  public Tab getTab() {
+    return myTab;
+  }
 
-    private void setupTab() {
-        VBox vbox = new VBox();
-        //Combo Boc for Selecting Image
-        ComboBox<String> setTurtleImage = new ComboBox<>();
-        setTurtleImage.setPromptText(resourceBundle.getString("LoadImage"));
-        ObservableList<String> images = getTurtleImages();
-        setTurtleImage.itemsProperty().bind(new SimpleObjectProperty<>(images));
-        setTurtleImage.setOnAction(e-> saveFile(setTurtleImage.valueProperty().get()));
+  private void setupTab() {
+    VBox vbox = new VBox();
+    //Combo Boc for Selecting Image
+    ComboBox<String> setTurtleImage = new ComboBox<>();
+    setTurtleImage.setPromptText(resourceBundle.getString("LoadImage"));
+    ObservableList<String> images = getTurtleImages();
+    setTurtleImage.itemsProperty().bind(new SimpleObjectProperty<>(images));
+    setTurtleImage.setOnAction(e -> saveFile(setTurtleImage.valueProperty().get()));
 
-        //Color Picker for Background and Pen Color
-        ColorPicker penColor = new ColorPicker();
-        ColorPicker backgroundColor = new ColorPicker();
-        penColor.setOnAction(e->setPenColor(penColor.getValue()));
-        backgroundColor.setOnAction(e->setBackgroundColor(backgroundColor.getValue()));
-        HBox penColorBox = getColorPickerBox(resourceBundle.getString("PenColor"), penColor);
-        HBox backgroundColorBox= getColorPickerBox(resourceBundle.getString("BackgroundColor"), backgroundColor);
+    //Color Picker for Background and Pen Color
+    ColorPicker penColor = new ColorPicker();
+    ColorPicker backgroundColor = new ColorPicker();
+    penColor.setOnAction(e -> setPenColor(penColor.getValue()));
+    backgroundColor.setOnAction(e -> setBackgroundColor(backgroundColor.getValue()));
+    HBox penColorBox = getColorPickerBox(resourceBundle.getString("PenColor"), penColor);
+    HBox backgroundColorBox = getColorPickerBox(resourceBundle.getString("BackgroundColor"),
+        backgroundColor);
 
-        //Select Language
-        List<String> contents = getLanguages();
-        ComboBox<String> languageBox = new ComboBox<>();
-        languageBox.setPromptText(resourceBundle.getString("SelectLanguage"));
-        languageBox.getItems().addAll(contents);
-        languageBox.setOnAction(e-> changeLanguage(languageBox.valueProperty().get()));
+    //Select Language
+    List<String> contents = getLanguages();
+    ComboBox<String> languageBox = new ComboBox<>();
+    languageBox.setPromptText(resourceBundle.getString("SelectLanguage"));
+    languageBox.getItems().addAll(contents);
+    languageBox.setOnAction(e -> changeLanguage(languageBox.valueProperty().get()));
 
-        Button help = createButton(resourceBundle.getString("HelpButton"));
-        help.setOnAction(e -> handleHelpScreen());
-        vbox.getChildren().addAll(setTurtleImage, penColorBox, backgroundColorBox, languageBox, help);
-        vbox.setSpacing(10.0);
-        myTab.setContent(vbox);
-    }
+    Button help = createButton(resourceBundle.getString("HelpButton"));
+    help.setOnAction(e -> handleHelpScreen());
+    vbox.getChildren().addAll(setTurtleImage, penColorBox, backgroundColorBox, languageBox, help);
+    vbox.setSpacing(10.0);
+    myTab.setContent(vbox);
+  }
 
-    private ObservableList<String> getTurtleImages() {
-        File directoryPath = new File(HELP_IMAGES_PATH);
-        List<String> images = new ArrayList<>();
-        for (String s: directoryPath.list()){
-            if (s.contains("turtle"))
-                images.add(s);
-        }
-        Collections.sort(images);
-        return FXCollections.observableList(images);
+  private ObservableList<String> getTurtleImages() {
+    File directoryPath = new File(HELP_IMAGES_PATH);
+    List<String> images = new ArrayList<>();
+    for (String s : directoryPath.list()) {
+      if (s.contains("turtle")) {
+        images.add(s);
+      }
     }
+    Collections.sort(images);
+    return FXCollections.observableList(images);
+  }
 
-    private void handleHelpScreen() {
-        final Stage dialog = new Stage();
-        dialog.initModality(Modality.NONE);
-        ScrollPane scrollPane = new ScrollPane();
-        VBox helpImages = createHelpScreen();
-        scrollPane.setContent(helpImages);
-        Scene dialogScene = new Scene(scrollPane);
-        dialog.setScene(dialogScene);
-        dialog.setTitle(resourceBundle.getString("HelpWindow"));
-        dialog.show();
-    }
+  private void handleHelpScreen() {
+    final Stage dialog = new Stage();
+    dialog.initModality(Modality.NONE);
+    ScrollPane scrollPane = new ScrollPane();
+    VBox helpImages = createHelpScreen();
+    scrollPane.setContent(helpImages);
+    Scene dialogScene = new Scene(scrollPane);
+    dialog.setScene(dialogScene);
+    dialog.setTitle(resourceBundle.getString("HelpWindow"));
+    dialog.show();
+  }
 
-    private VBox createHelpScreen() {
-        VBox vbox = new VBox();
-        List<String> images = getHelpImages();
-        for (String image: images){
-            Image i = new Image(this.getClass().getClassLoader().getResourceAsStream(image));
-            ImageView im = new ImageView(i);
-            im.setFitHeight(IMAGE_HEIGHT);
-            im.setFitWidth(IMAGE_WIDTH);
-            vbox.getChildren().add(im);
-        }
-        return vbox;
+  private VBox createHelpScreen() {
+    VBox vbox = new VBox();
+    List<String> images = getHelpImages();
+    for (String image : images) {
+      Image i = new Image(this.getClass().getClassLoader().getResourceAsStream(image));
+      ImageView im = new ImageView(i);
+      im.setFitHeight(IMAGE_HEIGHT);
+      im.setFitWidth(IMAGE_WIDTH);
+      vbox.getChildren().add(im);
     }
-    private List<String> getHelpImages(){
-        File directoryPath = new File(HELP_IMAGES_PATH);
-        List<String> images = new ArrayList<>();
-        for (String s: directoryPath.list()){
-            if (s.contains("Help"))
-                images.add(s);
-        }
-        Collections.sort(images);
-        return images;
-    }
-    private List<String> getLanguages() {
-        File directoryPath = new File(LANGUAGE_PATH);
-        List<String> languages = new ArrayList<>();
-        for (String s: directoryPath.list()){
-            int index = s.indexOf(".");
-            String substring = s.substring(0, index);
-            languages.add(substring);
-        }
-        return languages;
-    }
+    return vbox;
+  }
 
-    private void changeLanguage(String s) {
-        this.language = s;
+  private List<String> getHelpImages() {
+    File directoryPath = new File(HELP_IMAGES_PATH);
+    List<String> images = new ArrayList<>();
+    for (String s : directoryPath.list()) {
+      if (s.contains("Help")) {
+        images.add(s);
+      }
     }
+    Collections.sort(images);
+    return images;
+  }
 
-    public String getLanguage(){
-        return language;
+  private List<String> getLanguages() {
+    File directoryPath = new File(LANGUAGE_PATH);
+    List<String> languages = new ArrayList<>();
+    for (String s : directoryPath.list()) {
+      int index = s.indexOf(".");
+      String substring = s.substring(0, index);
+      languages.add(substring);
     }
-    private void setBackgroundColor(Color value) {
-      notifyListeners(BACKGROUND_COLOR, backgroundColorData, backgroundColorData = value.toString());
-    }
+    return languages;
+  }
 
-    private void setPenColor(Color value) {
-      notifyListeners(PEN_COLOR, penColorData, penColorData = value.toString());
-    }
+  private void changeLanguage(String s) {
+    this.language = s;
+  }
 
-    private void saveFile(String str) {
-        notifyListeners(TURTLE_IMAGE, turtleImage, turtleImage=str);
-    }
+  public String getLanguage() {
+    return language;
+  }
 
-    private void showMessage (Alert.AlertType type, String message) {
-        new Alert(type, message).showAndWait();
-    }
+  private void setBackgroundColor(Color value) {
+    notifyListeners(BACKGROUND_COLOR, backgroundColorData, backgroundColorData = value.toString());
+  }
 
-    private HBox getColorPickerBox(String str, ColorPicker colorPicker) {
-        HBox hbox = new HBox();
-        Text text = new Text(str);
-        hbox.getChildren().addAll(text, colorPicker);
-        hbox.setSpacing(10.0);
-        return hbox;
-    }
+  private void setPenColor(Color value) {
+    notifyListeners(PEN_COLOR, penColorData, penColorData = value.toString());
+  }
+
+  private void saveFile(String str) {
+    notifyListeners(TURTLE_IMAGE, turtleImage, turtleImage = str);
+  }
+
+  private void showMessage(Alert.AlertType type, String message) {
+    new Alert(type, message).showAndWait();
+  }
+
+  private HBox getColorPickerBox(String str, ColorPicker colorPicker) {
+    HBox hbox = new HBox();
+    hbox.setPadding((new Insets(10, 5, 10, 5)));
+    Text text = new Text(str);
+    hbox.getChildren().addAll(text, colorPicker);
+    hbox.setSpacing(10.0);
+    text.getStyleClass().add("color-picker-box");
+    return hbox;
+  }
 
   private void notifyListeners(String property, String oldValue, String newValue) {
     for (PropertyChangeListener name : listener) {
@@ -192,11 +204,11 @@ public class SettingView {
     listener.add(newListener);
   }
 
-    private Button createButton( String text) {
-        Button button = new Button();
-        button.setText(text);
-        return button;
-    }
+  private Button createButton(String text) {
+    Button button = new Button();
+    button.setText(text);
+    return button;
+  }
 
 
 }
