1495306612c8fc588ff87f9b065aeb8731e00eed
Milan Bhat <mb554@duke.edu>
2020-03-01 17:03:22 -0500

    Created class to implement backend external API for the controller to call on

--- src/slogo/ModelExternal.java
+++ src/slogo/ModelExternal.java
@@ -1,4 +1,22 @@
 package slogo;
 
-public class ModelExternal {
+import view.Visualizer;
+
+import java.util.List;
+
+public class ModelExternal implements BackEndExternal{
+
+    CommandManager commandManager;
+    String language;
+
+    public ModelExternal(String lang, MethodExplorer me, VariableExplorer ve, Visualizer v){
+        commandManager = new CommandManager(v, me, ve, lang);
+        language = lang;
+    }
+
+    @Override
+    public List<ImmutableTurtle> parseCommands(String input) throws ParsingException {
+        Parser parser = new Parser(language, commandManager);
+        return parser.parseCommands(input);
+    }
 }
