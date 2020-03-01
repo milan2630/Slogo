80d44aab8cd64c64b770c6e356ac29238839e3aa
Milan Bhat <mb554@duke.edu>
2020-03-01 17:11:28 -0500

    Created abstract class to have helpful, repeated command methods

---
+++ src/slogo/Commands/BackEndCommand.java
@@ -0,0 +1,30 @@
+package slogo.Commands;
+
+import slogo.CommandManager;
+import slogo.ParsingException;
+import slogo.VariableExplorer;
+
+import java.util.List;
+
+public abstract class BackEndCommand implements Command {
+
+    public abstract double executeCommand(CommandManager commandManager, List<String> params) throws ParsingException;
+
+    public double getDoubleParameter(String val, VariableExplorer variableExplorer) throws ParsingException {
+        try{
+            return Double.parseDouble(val);
+        }
+        catch (NumberFormatException e){
+            if(variableExplorer.getVariable(val) != null){
+                return (Double) variableExplorer.getVariable(val).getValue();
+            }
+            else if(val.indexOf(":") == 0){
+                variableExplorer.addDoubleVarByName(val, 0.0);
+                return 0.0;
+            }
+            else{
+                throw new ParsingException("UnrecognizedEntity", val);
+            }
+        }
+    }
+}
