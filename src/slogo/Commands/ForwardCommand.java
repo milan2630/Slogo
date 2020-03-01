61e9f43a35884bed38aa2732648170ac7b3497a6
Milan Bhat <mb554@duke.edu>
2020-03-01 17:04:41 -0500

    Added an execute method

--- src/slogo/Commands/ForwardCommand.java
+++ src/slogo/Commands/ForwardCommand.java
@@ -1,10 +1,41 @@
 package slogo.Commands;
 
+import slogo.*;
+
+import java.util.List;
+
 public class ForwardCommand implements Command {
 
     @Override
     public int getNumArguments() {
         return 1;
+    }
+
+    public double executeCommand(CommandManager commandManager, List<String> params) throws ParsingException {
+        Double pixForward = getDoubleParameter(params.get(0), commandManager.getVariableExplorer());
+        Turtle myTurtle = commandManager.getTurtle();
+        myTurtle.incrementX(pixForward * Math.sin(Math.toRadians(myTurtle.getHeading())));
+        myTurtle.incrementY(pixForward * Math.cos(Math.toRadians(myTurtle.getHeading())));
+        commandManager.getInternalStates().add(myTurtle.getImmutableTurtle());
+        return pixForward;
+    }
+
+    private double getDoubleParameter(String val, VariableExplorer variableExplorer) throws ParsingException {
+        try{
+            return Double.parseDouble(val);
+        }
+        catch (NumberFormatException e){
+            if(variableExplorer.getVariable(val) != null){
+                return (Double) variableExplorer.getVariable(val).getValue();
+            }
+            else if(val.indexOf(":") == 0){
+                variableExplorer.addDoubleVarByName(val, 0.0);
+                return 0.0;
+            }
+            else{
+                throw new ParsingException("UnrecognizedEntity", val);
+            }
+        }
     }
 
 }
