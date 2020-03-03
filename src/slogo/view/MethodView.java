616dec362aca7e74cd5eeaa88aad5821b44256b8
Ryan Weeratunga <rkw14@duke.edu>
2020-03-03 05:39:07 -0500

    added dialog box for method input (incomplete implementation)

--- src/slogo/view/MethodView.java
+++ src/slogo/view/MethodView.java
@@ -2,16 +2,16 @@ package slogo.view;
 
 import javafx.collections.*;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Tab;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.util.Pair;
 import slogo.Model.Commands.ControlStructures.UserDefinedInstructionCommand;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MethodView {
@@ -47,21 +47,44 @@ public class MethodView {
 
     private void displayMethod() {
         String s = listView.getSelectionModel().getSelectedItem();
-        int space = s.indexOf(" ");
+        int space = s.indexOf('\t');
         String methodName = s.substring(0, space);
-        String methodArgs = s.substring(space);
-        String args = methodArgs.replace(" ", "");
-        int parameters = Integer.parseInt(args);
-        String display = determineDisplay(methodName, parameters);
-        System.out.println("Selected");
-
+        String methodArgs = s.substring(space+1);
+        int parameters = Integer.parseInt(methodArgs);
+        if (parameters > 0 ) {
+            String display = determineDisplay(methodName, parameters);
+            displayDialogBox(display, parameters);
+        }
+        //to green [ ] [ fd 50 ]
     }
+
+    private void displayDialogBox(String display, int parameters) {
+        Dialog dialog = new Dialog();
+        //TODO make not hard coded
+        VBox vbox = new VBox();
+        for (int i =0; i<parameters; i++){
+            TextField input = new TextField();
+            int j = i+1;
+            input.setPromptText("Parameter "+j);
+            vbox.getChildren().add(input);
+        }
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        dialog.setTitle("o7planning");
+        dialog.setHeaderText(display);
+        vbox.setSpacing(10);
+        dialog.getDialogPane().setContent(vbox);
+//        Optional<List<String>> result = dialog.showAndWait();
+//        result.ifPresent(name -> {
+//            System.out.println(name);
+//        });
+    }
+
     private String determineDisplay(String name, int parameters){
         String result = name+ " [ ";
         for (int i=0; i< parameters; i++){
             result +="? ";
         }
-        result = "]";
+        result += "]";
         return result;
     }
     private void handle(MapChangeListener.Change<? extends String,? extends UserDefinedInstructionCommand> c) {
