632ff670f26acab42553717622912c9fe8b270a0
Robert Chen <rec43@duke.edu>
2020-03-04 21:43:09 -0500

    TurtleQueries commands implemented

--- src/slogo/Model/Commands/TurtleQueries/YCoordinateCommand.java
+++ src/slogo/Model/Commands/TurtleQueries/YCoordinateCommand.java
@@ -2,14 +2,13 @@ package slogo.Model.Commands.TurtleQueries;
 
 import slogo.Model.Parsing.CommandManager;
 import slogo.Model.Commands.BackEndCommand;
-import slogo.Model.ErrorHandling.ParsingException;
 import slogo.Model.TurtleModel.Turtle;
 
 import java.util.List;
 
 public class YCoordinateCommand extends BackEndCommand {
     @Override
-    public double executeCommand(CommandManager commandManager, Turtle myTurtle, List<String> params) throws ParsingException {
+    public double executeCommand(CommandManager commandManager, Turtle myTurtle, List<String> params) {
         return myTurtle.getY();
     }
 
